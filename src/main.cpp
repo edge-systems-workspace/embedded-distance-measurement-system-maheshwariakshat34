60a8ad625ca1bc12c617fa7b3ce0c4d69e09ca54
maheshwariakshat34 <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-02-28 10:54:19 +0530

    Implement ultrasonic trigger pulse and read echo duration

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,15 @@ void setup() {
     pinMode(echoPin, INPUT);
     Serial.begin(9600);
 }
+void loop() {
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+}
